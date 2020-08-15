042e3955b74570b0a532880ccb5ed19666167433
Protiven <protiven98@gmail.com>
2020-08-15 23:39:39 +0700

    Fixed Express

--- Week_1/node-express/index.js
+++ Week_1/node-express/index.js
@@ -13,31 +13,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
  
 
-
 app.use('/dishes', dishRouter);
-/*
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end(`Will send details of the dish: ` + req.params.dishId);
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
-});
-
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId);
-    res.end('Will update the dish: ' + req.params.dishId + ' with details: ' + req.body.decription);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting the dish: ' + req.params.dishId) ;
-});
-*/
-
 app.use(express.static(__dirname + '/public'));
-
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
