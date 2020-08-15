ab574402b5ac4de8cbfcf752cafc2ed2fc43b1aa
Protiven <protiven98@gmail.com>
2020-08-15 23:36:29 +0700

    modified dishRouter.js

--- Week_1/node-express/routes/dishRouter.js
+++ Week_1/node-express/routes/dishRouter.js
@@ -25,4 +25,25 @@ dishRouter.route('/')
     res.end('Deleting all the dishes!') ;
 });
 
+
+dishRouter.route('/:dishId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end('Will send the dish: ' + req.params.dishId);
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end("POST not supported in dishes/" + req.params.dishId);
+})
+.put((req, res, next) => {
+    res.end('The dish changed: ' + req.params.dishId) ;
+})
+.delete((req, res, next) => {
+    res.end('The dish deleted: ' + req.params.dishId) ;
+});
+
 module.exports = dishRouter;
