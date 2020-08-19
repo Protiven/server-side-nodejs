263fa72ca4fb5ea278c3feccf35c0fa499682697
Protiven <protiven98@gmail.com>
2020-08-19 19:20:21 +0700

    don't working

--- Week_2/node-Mongoose/index.js
+++ Week_2/node-Mongoose/index.js
@@ -6,42 +6,44 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-
     console.log('Connected correctly to server');
 
-    Dishes.create({
-        name: 'Uthappizza',
-        description: 'test'
-    })
-    .then((dish) => {
-        console.log(dish);
 
-        return Dishes.findByIdAndUpdate(dish._id, {
-            $set: { description: 'Updated test'}
-        }, {
-            new: true
-        }).exec();
-    })
-    .then((dish) => {
-        console.log(dish);
-
-        dish.comments.push({
-            reting: 5,
-            comment: 'So good!',
-            author: "Slavik"
-        });
-
-        return dish.save();
-    })
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.remove({});
-    })
+    Dishes.deleteOne({name: 'Uthappizza'})
     .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        Dishes.create({
+            name: 'Uthappizza',
+            description: 'test'
+        })
+        .then((dish) => {
+            console.log(dish);
 
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Updated test'}
+            }, {
+                new: true
+            }).exec();
+        })
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                reting: 5,
+                comment: 'So good!',
+                author: "Slavik"
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    });
 });
