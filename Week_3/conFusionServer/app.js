22ebf155384691f14f1b7d92c1c5d916d24eaae8
Protiven <protiven98@gmail.com>
2020-09-13 20:43:09 +0700

    Session 1

--- Week_3/conFusionServer/app.js
+++ Week_3/conFusionServer/app.js
@@ -46,7 +46,7 @@ app.use(session({
 function auth (req, res, next) {
   console.log(req.session);
 
-  if(!req.session) {
+  if(!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -60,17 +60,17 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-      res.cookie('user', 'admin', { signed: true});
-        next(); // authorized
+      req.session.user = 'admin';
+      next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
         res.setHeader('WWW-Authenticate', 'Basic'); 
         console.log("Not allowed for " + user);     
-        err.status = 401;reateError(404)
+        err.status = 401;
     }
   } 
   else {
-    if(req.signedCookies.user === 'admin' ){
+    if(req.session.user === 'admin' ){
       next(); /// 
     }
     else{
